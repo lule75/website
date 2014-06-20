60b810a10d20361aa42e5df9638a62561dfd2a47
Pedro Charun <mlosail3@gmail.com>
2014-06-20 16:46:00 -0500

    Agregados los modulos necesarios para la configuracion del mail en el sitio.

--- source/pecano.dev/sites/all/themes/Pecano_web/js/my_script.js
+++ source/pecano.dev/sites/all/themes/Pecano_web/js/my_script.js
@@ -1,7 +1,73 @@
-$("#datos-usuarios-link").hover(function  () {
-	$('datos-usuario').addClass('mostrar').removeClass('ocultar');
-},function () {
+jQuery(document).ready(function ($) {
+	$("#datos-usuarios-li").hover(function  () {
+		$('#datos-usuario').addClass('mostrar').removeClass('ocultar');
+	},function () {
+		$('#datos-usuario').addClass('ocultar').removeClass('mostrar');
+	});
+	$("#datos-usuarios-li").click(function(){
+		$('#datos-usuario').addClass('mostrar').removeClass('ocultar');
+	});
+	 function stickymenu() {
+
+		if($("body").hasClass("boxed"))
+			return false;
+
+		var headerHeight = $("body > header").height();
+		var logo = $("header .logo img");
+		var $this = this;
+		var logoSmallHeight = 50;
+
+		$this.checkStickyMenu = function() {
+
+			if($(window).scrollTop() > headerHeight + logoSmallHeight && $(window).width() > 768) {
+
+				if($("body").hasClass("sticky-menu-active"))
+					return false;
+
+				$("body").addClass("sticky-menu-active");
+				logo
+					.height(logoSmallHeight)
+					.css("width", "auto");
+
+			} else {
+
+				$("body").removeClass("sticky-menu-active");
+				logo
+					.css("height", "auto")
+					.css("width", "auto");
+
+			}
+
+		}
+
+		$(window).on("scroll", function() {
+
+			$this.checkStickyMenu();
+
+		});
+
+		$(window).on("resize", function() {
+
+			$this.checkStickyMenu();
+
+		});
+
+		$this.checkStickyMenu();
+
+		// Anchors Position
+		$("a[data-hash]").on("click", function(e) {
+
+			e.preventDefault();
+			var target = $(this.hash);
+
+			if(target.get(0))
+				$("html,body").animate({scrollTop: target.offset().top - (150)}, 300);
+
+			return false;
+
+		});
+		
+	}
+	
+	stickymenu();
 });
-$('#datos-usuario-cerrar').onClick(
-	$('datos-usuario').addClass('ocultar').removeClass('mostrar');
-);
